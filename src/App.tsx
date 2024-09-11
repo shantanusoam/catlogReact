69f157606e2e8ba7ff874115c458839cf946973d
shantanusoam <bluestackssoam@gmail.com>
2024-09-12 00:35:09 +0530

    resolved errors

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,4 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
-import { CryptoChart } from './components/crypto-chart'
 import CryptoDashboardJsx  from './components/crypto-dashboard2'
 
 
